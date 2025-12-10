bc3f2d8e070b8dc3c563c31adc8f8041574b8d9e
Larsi <44375174+Larsihasi98@users.noreply.github.com>
2025-12-10 23:32:05 +0100

    Day10 WIP

---
+++ Day10/Task02.py
@@ -0,0 +1,27 @@
+import os
+import re
+import math
+
+day = # Input day here
+
+test = False # Change this to run program with the smaller testinput for debug
+DEBUG = False # Change this to have debug outputs
+
+root = os.getcwd()
+if(os.path.basename(root) != 'AdventOfCode2025'):
+    root = os.path.normpath(os.path.join(root, '..'))
+current_day = os.path.join(root, f"Day{day:2d}")
+if test:
+    input_path = os.path.join(current_day, 'test.txt')
+else:
+    input_path = os.path.join(current_day, 'Input.txt')
+output_path = os.path.join(current_day, 'Output.txt')
+
+out = ''
+
+with open(input_path, 'r') as file_in:
+    input = file_in.read()
+
+
+with open(output_path, 'w') as file_out:
+    file_out.write(output_path)
